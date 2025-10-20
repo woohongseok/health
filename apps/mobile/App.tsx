735b65153ec06c64062e8ffcf5de30cf04493f2f
woohongseok <128017162+woohongseok@users.noreply.github.com>
2025-10-20 21:29:28 +0900

    chore: nativewind setting

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,20 +1,15 @@
+import './global.css';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style='auto' />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,10 @@
+export default function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+    plugins: ['react-native-reanimated/plugin'],
+  };
+}
