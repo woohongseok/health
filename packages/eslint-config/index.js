5fab8f93fabe49bacd8726f7a360105f3a08924c
woohongseok <128017162+woohongseok@users.noreply.github.com>
2025-10-21 12:42:26 +0900

    Chore/monorepo tsconfig settings (#6)
    * chore: centralization eslint settings
    
    * chore: centralization settings
    
    * chore: setting RN, next

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
