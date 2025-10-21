f4aeb69f31d61015a37dee3733b4d0e13b949140
woohongseok <128017162+woohongseok@users.noreply.github.com>
2025-10-21 12:58:17 +0900

    chore: centralization prettier settings (#7)

---
+++ packages/prettier-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
